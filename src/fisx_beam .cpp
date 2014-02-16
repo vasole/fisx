057804549a896dc58e897b33001a20692d771822
V. Armando Sole <sole@esrf.fr>
2014-02-16 11:26:35 +0100

    Class describing the excitation beam.

---
+++ src/fisx_beam .cpp
@@ -0,0 +1,102 @@
+#include "fisx_beam.h"
+#include <algorithm>
+
+Beam::Beam()
+{
+    this->normalized = false;
+}
+
+void Beam::setBeam(int nValues, double *energy, double *weight,
+                  double *characteristic, double *divergency)
+{
+    int i;
+    double tmpDouble;
+
+    this->normalized = false;
+    this->rays.clear();
+    this->rays.resize(nValues);
+
+    tmpDouble = 1.0;
+
+    for (i=0; i < nValues; ++i)
+    {
+        this->rays[i].energy = energy[i];
+        if (weight != NULL)
+        {
+            tmpDouble = weight[i];
+        }
+        this->rays[i].weight = tmpDouble;
+        if (characteristic == NULL)
+        {
+            this->rays[i].characteristic = 1.0;
+        }
+        else
+        {
+            this->rays[i].characteristic = characteristic[i];
+        }
+        if (divergency == NULL)
+        {
+            this->rays[i].divergency = 0.0;
+        }
+        else
+        {
+            this->rays[i].divergency = divergency[i];
+        }
+    }
+
+}
+
+void Beam::normalizeBeam()
+{
+    std::vector<Ray>::size_type nValues;
+    std::vector<Ray>::size_type i;
+    double totalWeight;
+
+    nValues = this->rays.size();
+    totalWeight = 0.0;
+
+    for (i = 0; i < nValues; ++i)
+    {
+        totalWeight += this->rays[i].weight;
+    }
+    if (totalWeight > 0.0)
+    {
+        for (i = 0; i < nValues; ++i)
+        {
+            this->rays[i].weight /= totalWeight;
+        }
+    }
+    this->normalized = true;
+    std::sort(this->rays.begin(), this->rays.end());
+
+}
+
+std::vector<double> Beam::getBeam()
+{
+    std::vector<double>::size_type nItems;
+    std::vector<Ray>::const_iterator c_it;
+    std::vector<double> returnValue;
+    Ray ray;
+
+
+    if (!this->normalized)
+    {
+        this->normalizeBeam();
+    }
+    nItems = 4 * this->rays.size();
+    returnValue.resize(nItems);
+    nItems = 0;
+    for(c_it = this->rays.begin(); c_it != this->rays.end(); ++c_it)
+    {
+        ray = *c_it;
+        returnValue[nItems] = ray.energy;
+        nItems++;
+        returnValue[nItems] = ray.weight;
+        nItems++;
+        returnValue[nItems] = ray.characteristic;
+        nItems++;
+        returnValue[nItems] = ray.divergency;
+        nItems++;
+    }
+    return returnValue;
+}
---
+++ src/fisx_beam.h
@@ -0,0 +1,70 @@
+#ifndef FISX_BEAM_H
+#define FISX_BEAM_H
+#include <cstddef> // needed for NULL definition!!!
+#include <vector>
+
+
+class Ray
+{
+public:
+    Ray()
+    {
+        this->energy = 0.0;
+        this->weight = 0.0;
+        this->characteristic = 0.0;
+        this->divergency = 0.0;
+    }
+    double energy;
+    double weight;
+    double characteristic;
+    double divergency;
+    bool operator < (const Ray &b) const
+    {
+        if (energy < b.energy)
+            return true;
+        if (weight < b.weight)
+            return true;
+        return false;
+    };
+};
+
+/*!
+  \class Beam
+  \brief Class describing an X-ray beam
+
+   At this point a beam is described by a set of energies and weights. The characteristic flag just indicates if
+   it is an energy to be considered for calculation of scattering peaks.
+*/
+class Beam
+{
+public:
+    Beam();
+    /*!
+    Minimalist constructor.
+    */
+
+    /*! Beam description given as vectors
+    */
+    void setBeam(const std::vector<double> & energy, \
+                 const std::vector<double> & weight,\
+                 const std::vector<double> & characteristic,\
+                 const std::vector<double> & divergency);
+
+    /*! Easy to wrap interface functions
+    Except for the energy, you can use NULL pointers to use default values.
+    */
+    void setBeam(int nValues, double *energy, double *weight = NULL,
+                 double *characteristic = NULL, double *divergency = NULL);
+
+    /*! Returns a vector of length 4 * the number of rays describing the beam as:
+    [energy0, weight0, characteristic0, divergency0, energy1, weight1, ...]
+    */
+    std::vector<double> getBeam();
+
+private:
+    bool normalized;
+    void normalizeBeam(void);
+    std::vector<Ray> rays;
+};
+
+#endif //FISX_BEAM_H
