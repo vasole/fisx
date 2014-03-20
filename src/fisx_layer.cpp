aabe1afff43761285e902af6ac7271b1d7609513
V. Armando Sole <sole@esrf.fr>
2014-03-20 17:41:55 +0100

    layer handling.

---
+++ src/fisx_layer.cpp
@@ -0,0 +1,89 @@
+#include <math.h>
+#include <stdexcept>
+#include "fisx_layer.h"
+
+Layer::Layer(const std::string & name, const double & density, \
+             const double & thickness, const double & funnyFactor)
+{
+    this->name = name;
+    this->materialName = name;
+    this->density = density;
+    this->thickness = thickness;
+    this->funnyFactor = funnyFactor;
+    this->hasMaterial = false;
+}
+
+void Layer::setMaterial(const std::string & materialName)
+{
+    this->materialName = materialName;
+    this->hasMaterial = false;
+}
+
+void Layer::setMaterial(const Material & material)
+{
+    this->material = material;
+    if (this->density < 0.0)
+    {
+        this->density = this->material.getDefaultDensity();
+    }
+    if (this->thickness <= 0.0)
+    {
+        this->thickness = this->material.getDefaultThickness();
+    }
+    this->hasMaterial = true;
+}
+
+double Layer::getTransmission(const double & energy, Elements & elements)
+{
+    // The material might not have been defined in the  current Elements instance.
+    // However, its composition might be fine.
+    double muTotal;
+    double tmpDouble;
+    if (this->hasMaterial)
+    {
+        muTotal = elements.getMassAttenuationCoefficients(this->material.getComposition(), energy)["total"];
+    }
+    else
+    {
+        muTotal = elements.getMassAttenuationCoefficients(this->materialName, energy)["total"];
+    }
+    tmpDouble = this->density * this->thickness;
+    if(tmpDouble <= 0.0)
+    {
+        std::string msg;
+        msg = "Layer " + this->name + " thickness is " + elements.toString(tmpDouble) + " g/cm2";
+        throw std::runtime_error( msg );
+    }
+
+    return exp(-(tmpDouble * muTotal));
+}
+
+std::vector<double> Layer::getTransmission(const std::vector<double> & energy, Elements & elements)
+{
+    std::vector<double>::size_type i;
+    std::vector<double> tmpDoubleVector;
+    double tmpDouble;
+
+    tmpDouble = this->density * this->thickness;
+    if(tmpDouble <= 0.0)
+    {
+        std::string msg;
+        msg = "Layer " + this->name + " thickness is " + elements.toString(tmpDouble) + " g/cm2";
+        throw std::runtime_error( msg );
+    }
+
+    if (this->hasMaterial)
+    {
+        tmpDoubleVector = elements.getMassAttenuationCoefficients(this->material.getComposition(), energy)["total"];
+    }
+    else
+    {
+        tmpDoubleVector = elements.getMassAttenuationCoefficients(this->materialName, energy)["total"];
+    }
+    for (i = 0; i < tmpDoubleVector.size(); i++)
+    {
+        tmpDoubleVector[i] = exp(-tmpDouble * tmpDoubleVector[i]);
+    }
+    return tmpDoubleVector;
+}
+
---
+++ src/fisx_layer.h
@@ -0,0 +1,57 @@
+#ifndef FISX_LAYER_H
+#define FISX_LAYER_H
+#include <string>
+#include <vector>
+#include "fisx_elements.h"
+
+/*!
+  \class Layer
+  \brief Class containing the composition of a layer
+*/
+
+class Layer
+{
+public:
+
+    Layer(const std::string & name="", const double & density = 0.0,
+                                       const double & thickness = 0.0,
+                                       const double & funnyFactor = 1.0);
+
+    void setMaterial(const std::string & materialName);
+
+    void setMaterial(const Material & material);
+
+    std::string name;
+    double density;
+    double thickness;
+
+    /*!
+    Eventually get a handle to underlying material
+    */
+    const Material & getMaterial() {return this->material;};
+
+    /*!
+    Get the layer transmission at the given energy using the elements library
+    supplied.
+    If the material is not defined or cannot be handled, it will throw the
+    relevant error.
+    */
+    double getTransmission(const double & energy, Elements & elements);
+
+    /*!
+    Get the layer transmissions at the given energies using the elements library
+    supplied.
+    If the material is not defined or cannot be handled, it will throw the
+    relevant error.
+    */
+    std::vector<double> getTransmission(const std::vector<double> & energy, Elements & elements);
+
+
+private:
+    std::string materialName;
+    bool hasMaterial;
+    Material material;
+    double funnyFactor;
+};
+
+#endif //FISX_LAYER_H
