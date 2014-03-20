e3bb1ff9d0d653876f4e273484e17ffeb74ca890
V. Armando Sole <sole@esrf.fr>
2014-03-20 09:30:05 +0100

    Parse into vector working.

--- src/fisx_simpleini.h
+++ src/fisx_simpleini.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sstream>
 
 class SimpleIni
 {
@@ -13,6 +14,43 @@ public:
     const std::vector<std:: string> & getKeys();
     const std::map<std::string, std::string> & readKey(const std::string & key,
                                                        const std::string & defaultValue="");
+    template<typename T>
+    void parseStringAsSingleValue(const std::string & keyContent,\
+                                  T & destination,
+                                  const T & defaultValue);
+
+    template<typename T>
+    static void parseStringAsMultipleValues(const std::string & keyContent,
+                                            std::vector<T> & destination, const T & defaultValue)
+    {
+        std::stringstream ss(keyContent);
+        T result;
+        std::string item;
+        char delimiter;
+
+        delimiter = ',';
+        destination.clear();
+        while (std::getline(ss, item, delimiter))
+        {
+            if (SimpleIni::stringConverter(item, result))
+                destination.push_back(result);
+            else
+                destination.push_back(defaultValue);
+        }
+    };
+
+    template<typename T>
+    static bool stringConverter(const std::string& str, T & number)
+    {
+        std::istringstream i(str);
+        if (!(i >> number))
+        {
+            // Number conversion failed
+            return false;
+        }
+        return true;
+    };
+
 private:
     std::string fileName;
     std::map<std::string, std::map<std::string, std::string> > keyContents;
