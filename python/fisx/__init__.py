ad46c471ddc9eaf46bd9c4a5c1753f95b6d3d087
V. Armando Sole <sole@esrf.fr>
2014-03-30 00:11:36 +0100

    Add new wrapped classes.

--- python/fisx/__init__.py
+++ python/fisx/__init__.py
@@ -4,3 +4,5 @@ from ._fisx import PyEPDL97 as EPDL97
 from ._fisx import PyShell as Shell
 from ._fisx import PyElement as Element
 from ._fisx import PyElements as Elements
+from ._fisx import PyLayer as Layer
+from ._fisx import PyXRF as XRF
