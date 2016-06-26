d4d57b01c161455c6127b09c9063d2dadc841ddb
V. Armando Sole <sole@esrf.fr>
2016-06-26 17:54:53 +0200

    Get rid of not finished methods.

--- python/cython/_fisx.pyx
+++ python/cython/_fisx.pyx
@@ -805,15 +805,6 @@ cdef class PyMath:
         """
         return self.thisptr.erfc(x)
 
-    def hypermet(self, x, gaussArea, position, fwhm,
-                 shortTailArea=0.0, shortTailSlope=0.03,
-                 longTailArea=0.0, longTailSlope=1.0,
-                 stepHeight=0.0):
-        """
-        Calculate the HYPERMET function of x.
-        """
-        return self.thisptr.hypermet(x, gaussArea, position, fwhm,
-                                     shortTailArea, shortTailSlope, longTailArea, longTailSlope, stepHeight)
 #/*##########################################################################
 #
 # The fisx library for X-Ray Fluorescence
