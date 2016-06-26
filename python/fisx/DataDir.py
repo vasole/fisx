01f48deda9becdfe199081c8fb59db1a24212943
V. Armando Sole <sole@esrf.fr>
2016-06-26 17:08:38 +0200

    Get rid of useless (and prone to errors) if block.

--- python/fisx/DataDir.py
+++ python/fisx/DataDir.py
@@ -30,18 +30,6 @@ import os
 FISX_DATA_DIR = 'DATA_DIR_FROM_SETUP'
 # this is to be filled by the setup
 FISX_DOC_DIR = 'DOC_DIR_FROM_SETUP'
-# what follows is only used in frozen versions
-if not os.path.exists(FISX_DATA_DIR):
-    tmp_dir = os.path.dirname(__file__)
-    old_tmp_dir = tmp_dir + "dummy"
-    basename = os.path.basename(FISX_DATA_DIR)
-    FISX_DATA_DIR = os.path.join(tmp_dir, "fisx", basename)
-    while (len(FISX_DATA_DIR) > 14) and (tmp_dir != old_tmp_dir):
-        if os.path.exists(FISX_DATA_DIR):
-            break
-        old_tmp_dir = tmp_dir
-        tmp_dir = os.path.dirname(tmp_dir)
-        FISX_DATA_DIR = os.path.join(tmp_dir, basename)
 
 # this is used in build directory
 if not os.path.exists(FISX_DATA_DIR):
@@ -67,19 +55,6 @@ if not os.path.exists(FISX_DATA_DIR):
             print(txt)
     else:
         raise IOError('%s directory not found' % basename)
-
-# do the same for the directory containing HTML files
-if not os.path.exists(FISX_DOC_DIR):
-    tmp_dir = os.path.dirname(__file__)
-    old_tmp_dir = tmp_dir + "dummy"
-    basename = os.path.basename(FISX_DOC_DIR)
-    FISX_DOC_DIR = os.path.join(tmp_dir,basename)
-    while (len(FISX_DOC_DIR) > 14) and (tmp_dir != old_tmp_dir):
-        if os.path.exists(FISX_DOC_DIR):
-            break
-        old_tmp_dir = tmp_dir
-        tmp_dir = os.path.dirname(tmp_dir)
-        FISX_DOC_DIR = os.path.join(tmp_dir, basename)
 
 if not os.path.exists(FISX_DOC_DIR):
     tmp_dir = os.path.dirname(os.path.abspath(__file__))
